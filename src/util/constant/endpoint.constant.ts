619136ebbfb39420037b0b08e2c271c893ba38d4
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-15 14:59:44 +0500

    feat: removed unused endpoints

--- src/util/constant/endpoint.constant.ts
+++ src/util/constant/endpoint.constant.ts
@@ -11,17 +11,11 @@ export enum Endpoint {
   Bank = 'http://localhost:4000/banks/:id',
   Banks = 'http://localhost:4000/banks',
   UploadBankLogo = 'http://localhost:4000/banks/upload-logo/:id',
-  
+
   Company = 'http://localhost:4000/companies/:id',
   Companies = 'http://localhost:4000/companies',
   UploadCompanyLogo = 'http://localhost:4000/companies/upload-logo/:id',
-  
+
   Benefit = 'http://localhost:4000/benefits/:id',
   Benefits = 'http://localhost:4000/benefits',
-  
-  Discount = 'http://localhost:4000/discounts/:id',
-  Discounts = 'http://localhost:4000/discounts',
-  
-  Cashback = 'http://localhost:4000/cashbacks/:id',
-  Cashbacks = 'http://localhost:4000/cashbacks',
 }
