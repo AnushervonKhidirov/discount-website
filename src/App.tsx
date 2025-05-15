cd5dd67715b76109a31f1440646a88b5d0bd1426
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-15 14:45:57 +0500

    feat: new routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
+import '@ant-design/v5-patch-for-react-19';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
-import { Page } from '@constant/link.constant';
-
+import { BenefitType } from '@type/benefit.type';
 import HeaderLayout from '@layout/header/header.layout';
 import BenefitPage from '@pages/benefit/benefit.page';
-import LoginPage from '@pages/login/login.page';
+import LogInPage from '@pages/log-in/log-in.page';
+import SignUpPage from '@pages/sign-up/sign-up.page';
+
+import { Page } from '@constant/link.constant';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={Page.Login} element={<LoginPage />} />
-
         <Route element={<HeaderLayout />}>
           <Route path={Page.Home} element={<BenefitPage />} />
-          <Route path={Page.Discount} element={<BenefitPage type="DISCOUNT" />} />
-          <Route path={Page.Cashback} element={<BenefitPage type="CASHBACK" />} />
+          <Route path={Page.Discount} element={<BenefitPage type={BenefitType.DISCOUNT} />} />
+          <Route path={Page.Cashback} element={<BenefitPage type={BenefitType.CASHBACK} />} />
+          <Route path={Page.PromoCode} element={<BenefitPage type={BenefitType.PROMO_CODE} />} />
         </Route>
+
+        <Route path={Page.LogIn} element={<LogInPage />} />
+        <Route path={Page.SignUp} element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
