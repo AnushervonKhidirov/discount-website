5727e1bff3f00472f0830493c0323ed643b0b1a0
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-15 14:44:50 +0500

    feat: HttpError updated

--- src/util/error/http.error.ts
+++ src/util/error/http.error.ts
@@ -1,10 +1,14 @@
+import type { HttpExceptionInstance } from '@type/common.type';
 import { CustomError } from './custom.error';
 
 export class HttpError extends CustomError {
-  status: number;
+  statusCode: number;
 
-  constructor(status: number, error: string, message?: string | string[]) {
-    super(error, message);
-    this.status = status;
+  constructor({ statusCode, message, error }: HttpExceptionInstance) {
+    const err = error ?? (Array.isArray(message) ? message[0] : message);
+    const msg = error ? message : '';
+
+    super(err, msg);
+    this.statusCode = statusCode;
   }
 }
