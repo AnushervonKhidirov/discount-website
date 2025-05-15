92c766272604ec60ea15264a2f5bdea2b12e57ec
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-15 14:50:16 +0500

    feat: profile button updated

--- src/component/common/profile-btn/profile-btn.tsx
+++ src/component/common/profile-btn/profile-btn.tsx
@@ -2,10 +2,8 @@ import type { FC } from 'react';
 import type { AdditionalProps } from '@type/common.type';
 
 import { useUserStore } from '@store/user.store';
-import { NavLink } from 'react-router';
 import { Button, Avatar } from 'antd/es';
 import { UserOutlined } from '@ant-design/icons';
-import { Page } from '@constant/link.constant';
 
 import classNames from 'classnames';
 import classes from './profile-btn.module.css';
@@ -16,11 +14,9 @@ const ProfileBtn: FC<AdditionalProps> = ({ className }) => {
   return (
     user && (
       <Button className={classNames(classes.profile_btn, className)}>
-        <NavLink to={Page.Profile}>
-          <Avatar className={classes.avatar} icon={<UserOutlined />} shape="square" />
-          <div className={classes.username}>{user.username}</div>
-          <div className={classes.role}>{user.role.replaceAll('_', ' ').toLocaleLowerCase()}</div>
-        </NavLink>
+        <Avatar className={classes.avatar} icon={<UserOutlined />} shape="square" />
+        <div className={classes.username}>{user.username}</div>
+        <div className={classes.role}>{user.role.replaceAll('_', ' ').toLocaleLowerCase()}</div>
       </Button>
     )
   );
