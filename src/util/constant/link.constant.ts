5c698e4ba4eb8aeae521c7998c12b19da390087d
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-16 13:52:49 +0500

    feat: new page added

--- src/util/constant/link.constant.ts
+++ src/util/constant/link.constant.ts
@@ -1,9 +1,12 @@
 export enum Page {
   Home = '/',
+  Store = '/stores',
+
   Promotion = '/promotions',
   Discount = '/promotions/discounts',
   Cashback = '/promotions/cashback',
   PromoCode = '/promotions/promo-codes',
+
   LogIn = '/log-in',
   SignUp = '/sign-up',
 }
