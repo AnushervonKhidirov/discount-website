0685ac126d51cd5a59766f72f40eab51b071a15c
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-16 14:40:09 +0500

    fix: content component styles fixed

--- src/component/common/content/content.tsx
+++ src/component/common/content/content.tsx
@@ -4,8 +4,16 @@ import { AdditionalProps } from '@type/common.type';
 import classNames from 'classnames';
 import classes from './content.module.css';
 
-const Content: FC<AdditionalProps> = ({ className, children }) => {
-  return <div className={classNames(classes.content, className)}>{children}</div>;
+const Content: FC<AdditionalProps<{ fullHeight?: boolean }>> = ({
+  fullHeight,
+  className,
+  children,
+}) => {
+  return (
+    <div className={classNames(classes.content, className, { [classes.full_height]: fullHeight })}>
+      {children}
+    </div>
+  );
 };
 
 export default Content;
