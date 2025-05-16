2ef73fbb6da3910676860e65fd3ee36117bad454
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-16 15:40:59 +0500

    style: size type updated

--- src/component/common/grid/grid.tsx
+++ src/component/common/grid/grid.tsx
@@ -4,9 +4,9 @@ import type { AdditionalProps } from '@type/common.type';
 import classNames from 'classnames';
 import classes from './grid.module.css';
 
-type size = 'xSmall' | 'small' | 'medium' | 'large';
+type Size = 'xSmall' | 'small' | 'medium' | 'large';
 
-const Grid: FC<AdditionalProps<{ size?: size }>> = ({ children, className, size = 'medium' }) => {
+const Grid: FC<AdditionalProps<{ size?: Size }>> = ({ children, className, size = 'medium' }) => {
   return <div className={classNames(classes.grid, classes[size], className)}>{children}</div>;
 };
 
