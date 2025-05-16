57005f4ae10ecb11a5ae78c7dab1086b930408f5
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-16 13:53:40 +0500

    feat: query client added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import '@ant-design/v5-patch-for-react-19';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-import { PromotionType } from '@type/promotion.type';
 import HeaderLayout from '@layout/header/header.layout';
 import PromotionPage from '@pages/promotion/promotion.page';
 import LogInPage from '@pages/log-in/log-in.page';
@@ -9,24 +9,23 @@ import SignUpPage from '@pages/sign-up/sign-up.page';
 
 import { Page } from '@constant/link.constant';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<HeaderLayout />}>
-          <Route path={Page.Promotion} element={<PromotionPage />} />
-          <Route path={Page.Discount} element={<PromotionPage type={PromotionType.DISCOUNT} />} />
-          <Route path={Page.Cashback} element={<PromotionPage type={PromotionType.CASHBACK} />} />
-          <Route
-            path={Page.PromoCode}
-            element={<PromotionPage type={PromotionType.PROMO_CODE} />}
-          />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HeaderLayout />}>
+            <Route path={Page.Promotion + '/*'} element={<PromotionPage />} />
+            <Route path={Page.Store + '/*'} element={<div>Store page</div>} />
+          </Route>
 
-        <Route path={Page.LogIn} element={<LogInPage />} />
-        <Route path={Page.SignUp} element={<SignUpPage />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path={Page.LogIn} element={<LogInPage />} />
+          <Route path={Page.SignUp} element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
