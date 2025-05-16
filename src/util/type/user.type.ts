b022b9cc8a795fbc5f6ded7c9c9a2deb5c5408e8
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-16 20:57:39 +0500

    feat: user type updated

--- src/util/type/user.type.ts
+++ src/util/type/user.type.ts
@@ -1,10 +1,15 @@
+import type { Company } from './company.type';
+
 export type User = {
   id: number;
   username: string;
+  firstName: string | null;
+  lastName: string | null;
   role: Role;
   archived: boolean;
   createdAt: Date;
   updatedAt: Date;
+  companies: Company[] | null;
 };
 
 export type UpdateUserData = Pick<User, 'username' | 'role' | 'archived'>;
