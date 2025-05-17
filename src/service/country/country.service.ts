0d6dcaa236ce99b45168712a2b8e317f991e0741
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-17 20:33:24 +0500

    feat: country service added

---
+++ src/service/country/country.service.ts
@@ -0,0 +1,39 @@
+import type { Country } from '@type/country';
+import type { ReturnPromiseWithErr } from '@type/return-with-error.type';
+import type { HttpExceptionInstance } from '@type/common.type';
+
+import axios from 'axios';
+import { Endpoint } from '@constant/endpoint.constant';
+import { HttpError } from '@error/http.error';
+import { isHttpException, returnError } from '@helper/response.helper';
+
+export class CountryService {
+  async get(id: number): ReturnPromiseWithErr<Country> {
+    try {
+      const { data } = await axios.get<Country | HttpExceptionInstance>(
+        Endpoint.Country.replace(':id', id.toString()),
+        {
+          validateStatus: () => true,
+        },
+      );
+
+      if (isHttpException(data)) throw new HttpError(data);
+      return [data, null];
+    } catch (err) {
+      return returnError(err);
+    }
+  }
+
+  async getAll(): ReturnPromiseWithErr<Country[]> {
+    try {
+      const { data } = await axios.get<Country[] | HttpExceptionInstance>(Endpoint.Countries, {
+        validateStatus: () => true,
+      });
+
+      if (isHttpException(data)) throw new HttpError(data);
+      return [data, null];
+    } catch (err) {
+      return returnError(err);
+    }
+  }
+}
