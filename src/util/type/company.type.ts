f310a9fba9b433a4736f77408191c953312ecc98
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-17 20:32:43 +0500

    feat company types updated

--- src/util/type/company.type.ts
+++ src/util/type/company.type.ts
@@ -6,7 +6,15 @@ export type Company = {
   archived: boolean;
   verified: boolean;
   categoryId: number;
+  countryIds: number[];
   createdAt: Date;
   updatedAt: Date;
   userId: number;
 };
+
+export type CreateCompany = Pick<Company, 'name' | 'about' | 'countryIds' | 'categoryId'>;
+
+export type UpdateCompany = Pick<
+  Company,
+  'name' | 'about' | 'countryIds' | 'categoryId' | 'logoUrl'
+>;
