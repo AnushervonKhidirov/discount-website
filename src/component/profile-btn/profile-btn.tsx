6d8c2e3b2724d98cd170c8e6374e4e1f4e9a6b22
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-17 17:49:07 +0500

    fix: fix error ownerOnly property

--- src/component/profile-btn/profile-btn.tsx
+++ src/component/profile-btn/profile-btn.tsx
@@ -13,9 +13,13 @@ const ProfileBtn: FC<AdditionalProps<{ user: User }>> = ({ user, className }) =>
   const userName = user.firstName ?? user.username;
   const isCompanyOwner = !!user.companies && user.companies.length > 0;
   const filteredMenu = isCompanyOwner ? profileMenu : profileMenu.filter(item => !item.ownerOnly);
+  const menuItems = filteredMenu.map(item => {
+    delete item.ownerOnly;
+    return item;
+  });
 
   return (
-    <Dropdown menu={{ items: filteredMenu }}>
+    <Dropdown menu={{ items: menuItems }}>
       <Button className={classNames(classes.profile_btn, className)}>
         <Avatar className={classes.avatar} icon={<UserOutlined />} shape="square" />
         <div className={classes.username}>{userName}</div>
