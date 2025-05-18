2e2d31dc9762a69e385b9b2aa93725ae11e0dddf
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-18 09:54:43 +0500

    feat: Create company page added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import HeaderLayout from '@layout/header/header.layout';
-import PromotionPage from '@pages/promotion/promotion.page';
 import LogInPage from '@pages/log-in/log-in.page';
 import SignUpPage from '@pages/sign-up/sign-up.page';
+import PromotionPage from '@pages/promotion/promotion.page';
+import CreateCompanyPage from '@pages/create-company/create-company.page';
 import StorePage from '@pages/store/store.page';
 
 import { Page } from '@constant/link.constant';
@@ -20,6 +21,7 @@ function App() {
           <Route element={<HeaderLayout />}>
             <Route path={Page.Promotion + '/*'} element={<PromotionPage />} />
             <Route path={Page.Store + '/*'} element={<StorePage />} />
+            <Route path={Page.CreateCompany + '/*'} element={<CreateCompanyPage />} />
           </Route>
 
           <Route path={Page.LogIn} element={<LogInPage />} />
