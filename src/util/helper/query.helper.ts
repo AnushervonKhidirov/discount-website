cce38a759874757e925d77968be4b14a6a7940ec
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-18 09:29:23 +0500

    feat: query builder modified

--- src/util/helper/query.helper.ts
+++ src/util/helper/query.helper.ts
@@ -1,24 +1,42 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export function queryBuilder(options: any): string {
-  return `?${queryObjectToArray(options).join('&')}`;
+import type { QueryObject } from '@type/query.type';
+
+import dayjs from 'dayjs';
+
+export function queryBuilder(options?: QueryObject): string {
+  return options ? `?${queryObjectToArray(options).join('&')}` : '';
 }
 
-export function queryObjectToArray(options: any): string[] {
+export function queryObjectToArray(options: QueryObject): string[] {
   const queries: string[] = [];
 
-  if (options) {
-    const opt: any = options;
+  for (const key in options) {
+    if (typeof options[key] === 'undefined') continue;
 
-    for (const key in opt) {
-      if (typeof opt[key] === 'undefined') continue;
+    const value = queryValueTypeConverting(options[key]);
+    if (value) queries.push(`${key}=${value}`);
 
-      if (typeof opt[key] !== 'object') {
-        queries.push(`${key}=${opt[key]}`);
-      } else {
-        queries.push(...queryObjectToArray(opt[key]))
-      }
+    if (isQueryObject(options[key])) {
+      queries.push(...queryObjectToArray(options[key]));
     }
   }
 
   return queries;
+}
+
+function queryValueTypeConverting(value: unknown) {
+  let convertedValue: string | undefined;
+
+  if (value instanceof Date) convertedValue = dayjs(value).format('DD-MM-YYYY');
+  if (Array.isArray(value)) convertedValue = value.join(',');
+  if (typeof value === 'string') convertedValue = value;
+  if (typeof value === 'boolean' || typeof value === 'number' || typeof value === 'bigint') {
+    convertedValue = `${value}`;
+  }
+
+  return convertedValue;
+}
+
+function isQueryObject(query: unknown): query is QueryObject {
+  if (query && typeof query === 'object') return !!Object.keys(query).length;
+  return false;
 }
